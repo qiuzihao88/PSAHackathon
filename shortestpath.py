079bf60d55f6e122f4dec7d1d0492ef59139bf42
qiuzihao88 <qiuzihao88@gmail.com>
2024-10-12 17:07:55 +0800

    added nodes

---
+++ shortestpath.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+G = nx.Graph()
+
+G.add_node("SG1", name="PSA SINGAPORE")
+G.add_node("VD1", name="SP-PSA INTERNATIONAL PORT")
+G.add_node("VD2", name="TAN CANG QUE VO INLAND CONTAINER DEPOT")
+G.add_node("TH1", name="EASTERN SEA LAEM CHABANG TERMINAL")
+G.add_node("TH2", name="THAI CONNECTIVITY TERMINAL")
+G.add_node("ID1", name="NEW PRIOK CONTAINER TERMINAL 1")
+G.add_node("CN1", name="DALIAN CONTAINER TERMINAL")
+G.add_node("CN2", name="FUZHOU CONTAINER TERMINALS")
+G.add_node("CN3", name="GUANGZHOU CONTAINER TERMINAL")
+G.add_node("CN4", name="TIANJIN TERMINALS")
+G.add_node("CN5", name="LYG-PSA CONTAINER TERMINAL")
+G.add_node("CN6", name="BEIBU-GULF INTERNATIONAL CONTAINER TERMINAL")
+G.add_node("KR1", name="INCHEON CONTAINER TERMINAL")
+G.add_node("KR2", name="BUSAN TERMINALS")
+G.add_node("JP1", name="HIBIKI CONTAINER TERMINAL")
