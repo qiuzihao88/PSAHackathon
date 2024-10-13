cf09b8c6bbdd8aaf41191e2b1f0fec824ef57e0b
qiuzihao88 <qiuzihao88@gmail.com>
2024-10-13 11:42:08 +0800

    china railway system

--- shortestpath.py
+++ shortestpath.py
@@ -1,22 +1,15 @@
 import networkx as nx
+import pandas as pd
 
 G = nx.Graph()
 
-G.add_node("SG1", name="PSA SINGAPORE")
-G.add_node("VD1", name="SP-PSA INTERNATIONAL PORT")
-G.add_node("VD2", name="TAN CANG QUE VO INLAND CONTAINER DEPOT")
-G.add_node("TH1", name="EASTERN SEA LAEM CHABANG TERMINAL")
-G.add_node("TH2", name="THAI CONNECTIVITY TERMINAL")
-G.add_node("ID1", name="NEW PRIOK CONTAINER TERMINAL 1")
-G.add_node("CN1", name="DALIAN CONTAINER TERMINAL")
-G.add_node("CN2", name="FUZHOU CONTAINER TERMINALS")
-G.add_node("CN3", name="GUANGZHOU CONTAINER TERMINAL")
-G.add_node("CN4", name="TIANJIN TERMINALS")
-G.add_node("CN5", name="LYG-PSA CONTAINER TERMINAL")
-G.add_node("CN6", name="BEIBU-GULF INTERNATIONAL CONTAINER TERMINAL")
-G.add_node("KR1", name="INCHEON CONTAINER TERMINAL")
-G.add_node("KR2", name="BUSAN TERMINALS")
-G.add_node("JP1", name="HIBIKI CONTAINER TERMINAL")
+file_path = 'ChinaRailway.xlsx'
+df = pd.read_excel(file_path, engine='openpyxl')
 
-for node, data in G.nodes(data=True):
-    print(f"Node: {node}, Name: {data['name']}")
+destinations = df.iloc[2:38, 1]
+
+
+for ports in destinations:
+    G.add_node(ports)
+
+
